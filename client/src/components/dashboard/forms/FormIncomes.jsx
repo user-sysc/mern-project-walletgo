b1212312a118fc187958406f053c7914e714f1c6
user-sysc <riosjulio117@gmail.com>
2024-06-10 01:10:09 -0500

    handleDeleteIncome

--- client/src/components/dashboard/forms/FormIncomes.jsx
+++ client/src/components/dashboard/forms/FormIncomes.jsx
@@ -1,9 +1,11 @@
 import { useCategory } from "../../../context/categoryContext";
 import { useIncome } from "../../../context/incomeContext";
 import { useAuth } from "../../../context/authContext";
-import "../../../styles/form.css";
-import Swal from "sweetalert2";
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { useState, useEffect } from "react";
+import "../../../styles/form.css";
+import { format } from "date-fns";
+import Swal from "sweetalert2";
 
 import { PureComponent } from "react";
 import {
@@ -18,10 +20,11 @@ import {
 
 function FormIncomes() {
   const [id, setId] = useState("");
-  const [editar, setEditar] = useState(false);
-  const [filteredIncomes, setFilteredIncomes] = useState([]);
-  const [filterValue, setFilterValue] = useState("");
   const [error, setError] = useState("");
+  const [editar, setEditar] = useState(false);
+  const [filterValue, setFilterValue] = useState("");
+  const [filteredIncomes, setFilteredIncomes] = useState([]);
+  const [filterValueCategory, setFilterValueCategory] = useState("");
 
   const { createIncome, getIncome, incomes, deleteIncome, updateIncome } =
     useIncome();
@@ -34,7 +37,6 @@ function FormIncomes() {
     amount: "",
     category_id: "",
   });
-
   const clean = () => {
     setFormData({
       title: "",
@@ -62,10 +64,9 @@ function FormIncomes() {
       [name]: value,
     }));
   };
-
   const getCategoryName = (id) => {
     const categoria = categories.find((cat) => cat.id === id);
-    return categoria ? categoria.name_category : "Desconocida";
+    return categoria ? categoria.name_category : "";
   };
   const handleCreateIncome = async (e) => {
     e.preventDefault();
@@ -89,14 +90,9 @@ function FormIncomes() {
         icon: "success",
         background: "#12151E",
         confirmButtonColor: "#1DB13E",
-        timer: 2000,
+        timer: 3000,
       });
-      setFormData({
-        title: "",
-        description: "",
-        amount: "",
-        category_id: "",
-      });
+      clean();
       await getIncome();
     } catch (error) {
       setError(error.response.data.message);
@@ -110,6 +106,95 @@ function FormIncomes() {
         footer: `<p style="color: white;">${error.response.data.message}</p>`,
       });
     }
+  };
+  const handleDeleteIncome = (val) => {
+    Swal.fire({
+      title: '<strong style="color: white;">Confirmar eliminación</>',
+      html:
+        '<i style="color: white;">¿Realmente desea eliminar a <strong>' +
+        val.title +
+        "</strong>?</i>",
+      icon: "warning",
+      background: "#12151E",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteIncome(val.id)
+          .then(() => {
+            Swal.fire({
+              title: '<strong style="color: white;">Registro eliminado!</>',
+              html:
+                '<i style="color: white;">El Ingreso <strong>' +
+                val.title +
+                '</strong style="color: white;"> fue eliminado exitosamente!</i>',
+              icon: "success",
+              background: "#12151E",
+              confirmButtonColor: "#1DB13E",
+            });
+          })
+          .catch((error) => {
+            Swal.fire({
+              title: '<strong style="color: white;">¡ERROR!</strong>',
+              html: '<i style="color: white;">Ha ocurrido un error al intentar eliminar el ingreso</i>',
+              icon: "warning",
+              background: "#12151E",
+              confirmButtonColor: "#1DB13E",
+              timer: 3000,
+              footer: `<p style="color: white;">${error.response.data.message}</p>`,
+            });
+          });
+      }
+    });
+  };
+  const handleUpdateIncome = async (e) => {
+    e.preventDefault();
+    try {
+      await updateIncome(id, formData);
+      clean();
+      Swal.fire({
+        title: "<strong>Actualización exitosa!</strong>",
+        html:
+          "<i>El Ingreso <strong>" +
+          formData.title +
+          "</strong> fue actualizado con éxito! </i>",
+        icon: "success",
+      });
+      await getIncome();
+    } catch (error) {
+      setError(error.response.data.message);
+      Swal.fire({
+        icon: "error",
+        title: "ERROR",
+        text: "Parece que hubo un error al actualizar el ingreso!",
+        footer: '<a href="#">Intente más tarde</a>',
+      });
+    }
+  };
+  const handleFilterChangeIncome = (e) => {
+    const query = e.target.value.toLowerCase();
+    setFilterValue(e.target.value);
+    setFilteredIncomes(
+      incomes.filter(
+        (income) =>
+          income.title.toLowerCase().includes(query) ||
+          getCategoryName(income.id).toLowerCase().includes(query)
+      )
+    );
+  };
+  const handleFilterChangeCategory = (e) => {
+    const query = e.target.value.toLowerCase();
+    setFilterValueCategory(e.target.value);
+    setFilteredIncomes(
+      incomes.filter((income) =>
+        String(income.category_id).toLowerCase().includes(query)
+      )
+    );
+  };
+  const formatFecha = (fecha) => {
+    return format(new Date(fecha), "dd/MM/yyyy");
   };
   const data = [
     {
@@ -149,7 +234,6 @@ function FormIncomes() {
       fullMark: 150,
     },
   ];
-
   class Example extends PureComponent {
     render() {
       return (
@@ -256,10 +340,17 @@ function FormIncomes() {
                 type="text"
                 id="producto-filter"
                 name="producto-filter"
-                placeholder="Filtrar ingresos"
+                placeholder="Filtrar Ingresos"
                 autoComplete="off"
+                value={filterValue}
+                onChange={handleFilterChangeIncome}
               />
-              <select id="categoria-filter" name="categoria-filter">
+              <select
+                id="categoria-filter"
+                name="categoria-filter"
+                value={filterValueCategory}
+                onChange={handleFilterChangeCategory}
+              >
                 <option value="">Seleccione una categoría</option>
                 {categories.map((categoria) => (
                   <option key={categoria.id} value={categoria.id}>
@@ -282,11 +373,21 @@ function FormIncomes() {
                 {filteredIncomes.map((val, key) => {
                   return (
                     <tr key={val.id}>
-                      <td>{val.titulo}</td>
-                      <td>{val.descripcion}</td>
-                      <td>{val.monto}</td>
-                      <td>{getCategoryName(val.idCategoria)}</td>
-                      <td>{val.createdAT}</td>
+                      <td>{val.title}</td>
+                      <td>{val.description}</td>
+                      <td>{val.amount}</td>
+                      <td>{getCategoryName(val.category_id)}</td>
+                      <td>
+                        <button className="edit-button">
+                          <FaEdit />
+                        </button>
+                        <button
+                          className="delete-button"
+                          onClick={() => handleDeleteIncome(val)}
+                        >
+                          <FaTrash />
+                        </button>
+                      </td>
                     </tr>
                   );
                 })}
